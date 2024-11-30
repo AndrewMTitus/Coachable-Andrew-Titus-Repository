497245ec3c6420c38cafb8e0debe13a718ab74e9
Andrew Titus <112717415+AndrewMTitus@users.noreply.github.com>
2024-11-30 12:41:43 -0500

    Update 27-remove-element.py
    updated code

--- leetcode/27-remove-element.py
+++ leetcode/27-remove-element.py
@@ -8,13 +8,13 @@ def removeElement(nums, val):
     k = 0, 1, 3, 0, 4
     k = 5
     """
-    k = 0
+    k = 0 #pointer for the next position of a non-val element
 
-    for i in range(len(nums)):
-        if nums[i] != val:
-            nums[k] = nums[i]
-            k += 1
+    for num in nums:
+        if num != val:
+            nums[k] = num #Keep the element
+            k += 1 #Increment the pointer of non-val elements.
     return k
-
+    
     #time O(n)
     #space 0(1)
