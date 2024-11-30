13654d11acdd2d881296c6c91dadaa34016a6fb4
Andrew Titus <112717415+AndrewMTitus@users.noreply.github.com>
2024-11-30 13:11:23 -0500

    Create 13_Roman_To_Integer.py
    13.
    
    https://www.loom.com/share/cf1b36feb98f466ba939cae384b1495a?sid=51efd31e-9d9e-427c-babd-ec4bc1f1ff11
    
    We want to convert roman numerals into integers.
    The key idea is to iterate through the string and if the position in front of the previous one is greater, then we minus it, if not then we add it to the total.

---
+++ leetcode/13_Roman_To_Integer.py
@@ -0,0 +1,25 @@
+def romanToInt(s):
+    """
+    :type s: str
+    :rtype: int
+    s = "MCMXCIV"
+    dict 
+    1000 900 1900 90 1990 4 1994
+    """
+    roman = {
+        "I": 1,
+        "V": 5,
+        "X": 10,
+        "L": 50,
+        "C": 100,
+        "D": 500,
+        "M": 1000
+    }
+    total = 0
+    for i in range(len(s)):
+        if i == len(s) - 1 or roman[s[i]] >= roman[s[i + 1]]:
+            total += roman[s[i]]
+        else:
+            total -= roman[s[i]]
+    return total
+    #time O(n) space O(1)
