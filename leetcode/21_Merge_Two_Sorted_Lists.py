f32a64763e3fce9f5e9de4d7d898f7f45d90b799
Andrew Titus <112717415+AndrewMTitus@users.noreply.github.com>
2024-11-30 13:38:28 -0500

    Create 21_Merge_Two_Sorted_Lists.py
    21.
    
    We are given the heads of two linked lists, we want to merge the two lists together by splicing the first node of each list.
    We can iterate through both lists simultaneously and store them in a new list called dummy. We always add the smaller node first. If there are any remaining nodes after traversing we add them to the dummy list. We then return the next node of dummy, which is the head of the new list.

---
+++ leetcode/21_Merge_Two_Sorted_Lists.py
@@ -0,0 +1,32 @@
+def mergeTwoLists(list1, list2):
+    """
+    :type list1: Optional[ListNode]
+    :type list2: Optional[ListNode]
+    :rtype: Optional[ListNode]
+    we can iterate through both lists simultaneously and
+    add the nodes onto a new list. we compare nodes and 
+    whichever node is smaller gets added first. 
+    """
+    #create a dummy list to serve as the starting point
+    #of the merged list
+    dummy = ListNode(0)
+    current = dummy
+
+    #Loop until one of the lists is empty
+    while list1 and list2:
+        #choose the smaller value node to add to the
+        #merged list.
+        if list1.val <= list2.val:
+            current.next = list1
+            list1 = list1.next
+        else:
+            current.next = list2
+            list2 = list2.next
+        current = current.next
+        #if there are any remaining nodes attach them
+        current.next = list1 if list1 else list2
+        #return the next node of the dummy list, which is
+        #the head of the merged list
+        return dummy.next
+        #time O(m + n) since we iterate through 2 lists.
+        #space O(1) no extra space used.
