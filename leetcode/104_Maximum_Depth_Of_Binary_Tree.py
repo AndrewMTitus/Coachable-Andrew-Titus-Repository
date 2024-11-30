0e2870c42567fc9113f94f57b870d6babb1581df
Andrew Titus <112717415+AndrewMTitus@users.noreply.github.com>
2024-11-30 13:41:00 -0500

    Create 104_Maximum_Depth_Of_Binary_Tree.py
    104.
    
    https://leetcode.com/problems/maximum-depth-of-binary-tree/description/
    
    We are given the root of a binary tree and want to find its maximum depth.
    The key idea is to use recursion to go as deep as possible in the tree and use max to find which depth is greater out of the left and right. We add 1 to the answer to get the correct height.

---
+++ leetcode/104_Maximum_Depth_Of_Binary_Tree.py
@@ -0,0 +1,16 @@
+def maxDepth(root):
+    """
+    :type root: Optional[TreeNode]
+    :rtype: int
+    if the node is None, the depth is 0
+    recursion to find the depth of left and right
+    add 1 to our max depth
+    """
+    if not root:
+        return 0
+    left_depth = self.maxDepth(root.left)
+    right_depth = self.maxDepth(root.right)
+
+    return 1 + max(left_depth, right_depth)
+    #time O(N) for number of nodes
+    #space O(H) for height of tree due to recursion stack
