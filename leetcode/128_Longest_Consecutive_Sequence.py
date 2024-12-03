cbf6b965cb172db9ad84623a7bc22ffdba218ca7
Andrew Titus <112717415+AndrewMTitus@users.noreply.github.com>
2024-12-03 12:22:54 -0500

    Update 128_Longest_Consecutive_Sequence.py
    fixed trailing whitespace

--- leetcode/128_Longest_Consecutive_Sequence.py
+++ leetcode/128_Longest_Consecutive_Sequence.py
@@ -9,7 +9,7 @@ def longestConsecutive(nums):
     """
     if not nums:
         return 0
-    
+        
     num_set = set(nums)
     longest_streak = 0
 
@@ -23,7 +23,6 @@ def longestConsecutive(nums):
             while current_num + 1 in num_set:
                 current_num += 1
                 current_streak += 1
-            
             longest_streak = max(longest_streak, current_streak)
 
     return  longest_streak
