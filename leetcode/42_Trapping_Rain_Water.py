a8ee3e89b9663207e7115b26f37305662f15465c
Andrew Titus <112717415+AndrewMTitus@users.noreply.github.com>
2024-12-03 13:02:06 -0500

    Update 42_Trapping_Rain_Water.py
    trailing whitespace

--- leetcode/42_Trapping_Rain_Water.py
+++ leetcode/42_Trapping_Rain_Water.py
@@ -6,7 +6,7 @@ def trap(height):
     """
     if not height:
         return 0
-    
+        
     left, right = 0, len(height) - 1
     left_max, right_max = 0, 0
     water = 0
@@ -24,7 +24,6 @@ def trap(height):
             else:
                 water += right_max - height[right]
             right -= 1
-    
     return water
 
     #time O(n) each element is accessed once.
