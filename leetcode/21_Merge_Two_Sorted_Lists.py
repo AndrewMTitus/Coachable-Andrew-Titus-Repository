de0c09f12df826a0fbf71f38bda44cf08d69e3bf
Andrew Titus <112717415+AndrewMTitus@users.noreply.github.com>
2024-12-03 12:53:37 -0500

    Update 21_Merge_Two_Sorted_Lists.py
    fixed issues with spacing

--- leetcode/21_Merge_Two_Sorted_Lists.py
+++ leetcode/21_Merge_Two_Sorted_Lists.py
@@ -1,3 +1,7 @@
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
 def mergeTwoLists(list1, list2):
     """
     :type list1: Optional[ListNode]
@@ -23,10 +27,11 @@ def mergeTwoLists(list1, list2):
             current.next = list2
             list2 = list2.next
         current = current.next
-        #if there are any remaining nodes attach them
-        current.next = list1 if list1 else list2
-        #return the next node of the dummy list, which is
-        #the head of the merged list
-        return dummy.next
-        #time O(m + n) since we iterate through 2 lists.
-        #space O(1) no extra space used.
+        
+    #if there are any remaining nodes attach them
+    current.next = list1 if list1 else list2
+    #return the next node of the dummy list, which is
+    #the head of the merged list
+    return dummy.next
+    #time O(m + n) since we iterate through 2 lists.
+    #space O(1) no extra space used.
