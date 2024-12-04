a0d6197fa77b9a09bb0e937c75be965795550c53
Andrew Titus <112717415+AndrewMTitus@users.noreply.github.com>
2024-12-04 13:08:29 -0500

    Update 19_Remove_Nth_Node_From_End_Of_List.py

--- leetcode/19_Remove_Nth_Node_From_End_Of_List.py
+++ leetcode/19_Remove_Nth_Node_From_End_Of_List.py
@@ -1,7 +1,7 @@
 class ListNode:
     def __init__(self, val=0, next=None):
         self.val = val
-        self.next = next
+        self.next = next_node
 def removeNthFromEnd(head, n):
     """
     :type head: ListNode
