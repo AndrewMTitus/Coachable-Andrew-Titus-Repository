3f79504f7dbddab24904d559b82f32bf6ad9fef0
Andrew Titus <112717415+AndrewMTitus@users.noreply.github.com>
2024-12-09 14:26:00 -0500

    Update 3_Longest_Substring_Without_Repeating_Characters.py

--- leetcode/3_Longest_Substring_Without_Repeating_Characters.py
+++ leetcode/3_Longest_Substring_Without_Repeating_Characters.py
@@ -11,23 +11,22 @@ and max_length stores the longest substring found
 window.
 6. return result
 """
-class Solution(object):
-    def lengthOfLongestSubstring(self, s):
-        """
-        :type s: str
-        :rtype: int
-        """
-        char_set = set()
-        left = 0
-        max_length = 0
+def lengthOfLongestSubstring(self, s):
+    """
+    :type s: str
+    :rtype: int
+    """
+    char_set = set()
+    left = 0
+    max_length = 0
 
-        for right in range(len(s)):
-            while s[right] in char_set:
-                char_set.remove(s[left])
-                left += 1
-            char_set.add(s[right])
-            max_length = max(max_length, right - left + 1)
-        return max_length
+    for right in range(len(s)):
+        while s[right] in char_set:
+            char_set.remove(s[left])
+            left += 1
+        char_set.add(s[right])
+        max_length = max(max_length, right - left + 1)
+    return max_length
 
-        #time O(n) each element accessed once
-        #space O(k) for the storage of unique characters.
+    #time O(n) each element accessed once
+    #space O(k) for the storage of unique characters.
